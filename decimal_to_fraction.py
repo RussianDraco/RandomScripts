983cb275c7e710754e73fe2fb61b11e48cf45e79
unknown <dracospy369@gmail.com>
2023-10-03 21:02:45 -0400

    dec_to_frac

--- decimal_to_fraction.py
+++ decimal_to_fraction.py
@@ -1,5 +1,11 @@
 print("Input decimal...")
-input_dec = input(">")
+in_dec = float(input(">"))
+
+def dec_point_before(sd):
+    
 
 #basic implementation lacking some specific/accurate fractions
 
+s_d = str(in_dec)
+
+while dec_point_before(s_d):
