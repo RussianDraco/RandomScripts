fe7368ffc648a411925915c6ce5c472c65ba3ee8
unknown <dracospy369@gmail.com>
2023-10-03 21:08:19 -0400

    dec_to_frac

--- decimal_to_fraction.py
+++ decimal_to_fraction.py
@@ -1,11 +1,19 @@
 print("Input decimal...")
 in_dec = float(input(">"))
 
-def dec_point_before(sd):
-    
+def fully_int(sd):
+    sdl = list(sd)
+    pp = sdl.index(".")
+    afterp = sdl[pp + 1:]
+    for x in afterp:
+        if x != "0":
+            return False
+    return True
 
 #basic implementation lacking some specific/accurate fractions
 
 s_d = str(in_dec)
 
-while dec_point_before(s_d):
+#while not fully_int(s_d):
+#    pp = list(s_d).index(".")
+#    s_d = s_d.replace(".", "")
