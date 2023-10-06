efdb4adbc6e279a18d0ad117db4af3e5215cf030
unknown <dracospy369@gmail.com>
2023-10-06 16:59:01 -0400

    binary_tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,10 +1,11 @@
 import random
 
 class Node:
-    def __init__(self, data, left = None, right = None):
+    def __init__(self, data, left = None, right = None, depth = 0):
         self.left = left
         self.right = right
         self.data = data
+        self.depth = depth
 
     def insert(self, data):
         if self.data == None:
@@ -12,21 +13,39 @@ class Node:
         else:
             if data < self.data:
                 if self.left == None:
-                    self.left = Node(data)
+                    self.left = Node(data, depth = self.depth + 1)
                 else:
                     self.left.insert(data)
             elif data > self.data:
                 if self.right == None:
-                    self.right = Node(data)
+                    self.right = Node(data, depth = self.depth + 1)
                 else:
                     self.right.insert(data)
 
+    def getChildren(self):
+        return ((self.left if self.left != None else None), (self.right if self.right != None else None))
+
     def printMyNodes(self):
-        print(self.data)
-        if self.left:
-            self.left.printMyNodes()
-        if self.right:
-            self.right.printMyNodes()
+        print_array = [self]
+        for x in range(self.findTreeHeight()):
+            for y in print_array[x]:
+                childs = y.getChildren()
+                if childs[0] != None:
+                    print_array[x + 1].append(childs[0])
+                if childs[1] != None:
+                    print_array[x + 1].append(childs[1])
+
+    def findTreeHeight(self):
+        if self.left == None:
+            l = self.depth
+        else:
+            l = self.left.findTreeHeight()
+        if self.right == None:
+            r = self.depth
+        else:
+            r = self.right.findTreeHeight()
+
+        return max(l, r)
 
 tree = Node(1000)
 
@@ -34,3 +53,5 @@ for x in range(0, 50):
     tree.insert(random.randint(0, 1000))
 
 tree.printMyNodes()
+
+print("Total height: " + str(tree.findTreeHeight()))
